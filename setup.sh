c31b0019fd60e99bb702a804408234c7643d0932
Justin Griffiths <jgriffi6@gmail.com>
2017-05-25 09:49:26 -0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,1 @@
+export PATH=/home/sg0228178/workarea/Shopping/bin:/home/sg0228178/local_builds/packages/bin:/home/sg0228178/R/bin/:/home/sg0228178/packages/R-dev-3.3.3/bin/:/opt/rbenv/shims:/opt/rbenv/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/sg0228178/bin:/home/sg0228178/MyLoginScripts/bin
